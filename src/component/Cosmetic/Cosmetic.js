a0fc1527b202c27ae61ff04720768ed054016361
mim2041 <suraiyamim338@gmail.com>
2023-03-30 20:46:45 +0600

    localStorage set by making a fakedb

--- src/component/Cosmetic/Cosmetic.js
+++ src/component/Cosmetic/Cosmetic.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { addToDb } from '../utilities/Fakedb';
 import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
     const addToCart = (id) => {
-        console.log('item added', id)
+        addToDb(id)
     }
 
-    const addToCartWithParam = () => addToCart(id);
-    
+    // const addToCartWithParam = () => addToCart(id);
+
     return (
         <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for: ${price}</p>
             <p><small>It has id: {id}</small></p>
-            <button onClick={addToCartWithParam}>Add to Cart</button>
-            <button onClick={() => addToCart(id)}>Purchase</button>
+            <button onClick={() => addToCart(id)}>Add to Cart</button>
+            {/* <button onClick={addToCartWithParam}>Add to Cart</button> */}
         </div>
     );
 };
