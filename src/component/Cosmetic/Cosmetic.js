30fdeda5a1170b32445fde8759130f2f0d082ddc
mim2041 <suraiyamim338@gmail.com>
2023-03-30 20:25:02 +0600

    wrapping a function with parameter for calling

--- src/component/Cosmetic/Cosmetic.js
+++ src/component/Cosmetic/Cosmetic.js
@@ -3,12 +3,19 @@ import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic;
+    const addToCart = (id) => {
+        console.log('item added', id)
+    }
+
+    const addToCartWithParam = () => addToCart(id);
+    
     return (
         <div className='product'>
             <h2>Buy this: {name}</h2>
             <p>Only for: ${price}</p>
             <p><small>It has id: {id}</small></p>
-            <button>Add to Cart</button>
+            <button onClick={addToCartWithParam}>Add to Cart</button>
+            <button onClick={() => addToCart(id)}>Purchase</button>
         </div>
     );
 };
